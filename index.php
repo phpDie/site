b04b9b8a74cfc361a368494be8277b5d89fce026
phpDie <zxc7613@mail.ru>
2018-02-22 23:15:42 +1000

    привеТ, неудачаники!

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+
+
+    <title>TRY</title>
+
+	TEST1
+	TEST2
+	TEST 3 \ новые изменения в файле
+	TEST 4 css add
+ 
+  </body>
+</html>
+	
---
+++ z.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+
+
+    <title>TRY</title>
+
+	TEST1
+ 
+  </body>
+</html>
+	
